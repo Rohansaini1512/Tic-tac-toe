54a7385a84c711ef32fd90819b6e76aa6b80ba2e
Rohansaini1512 <sainirohan830@gmail.com>
2023-11-21 19:48:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from './components/Card/Card'
+import Grid from './components/Grid/Grid'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Grid numberOfCards = {9} />
+    </>
+  )
+}
+
+export default App
